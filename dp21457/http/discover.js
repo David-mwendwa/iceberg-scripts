851ba6e4a1b914adfe979cc04819a31bdd70d483
David Mwendwa <klatzdavie19@gmail.com>
2021-10-08 21:40:36 +0300

    #dp21457: add discover links

---
+++ dp21457/http/discover.js
@@ -0,0 +1,14 @@
+function discoverLinks({content, contentType, canonicalURL, requestURL}) {
+    let links = [];
+  	if (/html/i.test(contentType)) {
+        const $ = cheerio.load(content);
+        $(".view-content a[href], .content a[href]").each(function () {
+            let href = $(this).attr('href');
+            href = href ? url.resolve(requestURL, href) : null;
+            if (href) {
+                links.push(href)
+            }
+        })
+    }
+    return links;
+}
