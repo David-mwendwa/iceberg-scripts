2ab2fcfcc22c2743e42d7cffcb49a44008e45fbe
David Mwendwa <klatzdavie19@gmail.com>
2021-10-18 09:40:33 +0300

    remove wrong placed code

--- dp21133/http/fetch2.js
+++ dp21133/http/fetch2.js
@@ -29,16 +29,3 @@ async function fetchURL({canonicalURL, headers}) {
         return defaultFetchURL({canonicalURL, headers});
     }
 }
-
-
-// Parser for connecting the ZIP and its content
-async function parsePage({ responseBody, URL, html, referer }) {
-  const $ = cheerio.load(responseBody.content);
-  const results = [];
-  let URI = [URL];
-  $('a[href*="doc"], a[href*="docx"], a[href*="pdf"]').each(function () {
-    let docURL = $(this).attr('href');
-    results.push({ URI: docURL, parentURL: URL });
-  });
-  return results;
-}
