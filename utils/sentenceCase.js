7a81c28b7a40378d6f15e945360a5e62830c42a5
David Mwendwa <klatzdavie19@gmail.com>
2021-10-27 17:22:23 +0300

    #utils: add string to _sentenseCase_ converter utility

---
+++ utils/sentenceCase.js
@@ -0,0 +1,8 @@
+const sentenceCase = (input) => {
+  input = input === undefined ? null : input;
+  return (
+    input && input.toString().toLowerCase().replace(/(^|[.?!] *)([a-z])/g,
+        (match, separator, char) => separator + char.toUpperCase()
+      )
+  );
+};
