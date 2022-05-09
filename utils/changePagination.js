bd3bc5d90b7cdc7b45b4e28e5bd48e94193e34e0
techdave <klatzdavie19@gmail.com>
2022-05-10 02:48:12 +0300

    utils: add on the util to change pagination

--- utils/changePagination.js
+++ utils/changePagination.js
@@ -1,13 +1,13 @@
 const cheerio = require('cheerio')
 
-const changePagination = async function ({ responsePage }) {
+const changePagination = async function ({ responsePage, canonicalURL }) {
 	  let html = await responsePage.response.text();
     let $ = cheerio.load(html)
   	$(".item-list > ul.pager > li > a").each(function (i) {
         let a = $(this)
         let pagination = a.attr('href')
         let page = +pagination.split('page=').pop() + 1
-        let newPagination = pagination.replace(/page=(\d+)/, `page=${page}`)
+        let newPagination = canonicalURL.replace(/page=(\d+)/, `page=${page}`);
         a.attr('href', newPagination)
     })
     responsePage.response = new fetch.Response($.html(), responsePage.response);
