bd5867ed4fbcaf654f7c00004fdb5a86d53e18dd
techdave <klatzdavie19@gmail.com>
2022-10-28 21:34:27 +0300

    dp16629: update pdf-split parser

--- dp16629/parsers/pdf-split.js
+++ dp16629/parsers/pdf-split.js
@@ -6,7 +6,7 @@ async function parsePage({URL, responseBody, html}) {
         return [];
     }
   	let results = []
-  
+
     let text = await runRemoteFilter({URL, filter: "pdftotext_raw"});
   	if (text && typeof text === "string"){
       let lines = text && text.split(/\s*\n\s*/ig).filter(l=>l);
@@ -23,35 +23,80 @@ async function parsePage({URL, responseBody, html}) {
         match = /\D+?(\d{1,2}(?: del?) \w+(?: del?) \d{4})/i.exec(textString)
         publicação = match && match[1]
       }
-      match = /SUMÁRIO(.+[\.]{30}\s?\d+)/i.exec(textString)
-      let summaryText = match && match[1]
-      //results.push({summaryText})
-      let splits = summaryText.split(/([a-z].+?[\.]{10}.+?\d+)/i).filter((l) => /^\w.+\d+/.test(l) && l);
-      //results.push({splits})
-      const sections = []
-      for (let i = 0; i < splits.length; i++) {
-        let d = {}
-        let title = splits[i]
-        let match = /[.\s]{2,}(\d+)/.exec(title);
-        if (match) {
-          if (/\.gov\.br/.test(title)) {
-          	title = title.replace(/^.+\.gov\.br/, '')
+      let sections = []
+      if (html) { // MUST USE filter: 'pdftohtmlExact'
+        const $ = cheerio.load(html);
+        let section = null
+        let subsection = null
+        $('div[id^="page"] p:has(a)').each(function () {
+          let styles = $(this).attr('style');
+          let match = /left\D+(\d+)/.exec(styles);
+          let pxLeft = match && parseInt(match[1]);
+          let a = $(this).find('a[href*="inputs.html"]');
+          let href = a.attr('href');
+          let text = a.text().trim();
+          let page = href.split('#').pop()
+          if (text && !/^[0-9\.]/.test(text)) {
+            if (pxLeft < 55) { // section
+              section = text;
+            } else if (pxLeft < 70) { //subsection
+              subsection = text
+              sections.push({ section, subsection, title: text, page });
+            } else { // title
+              sections.push({ section, subsection, title: text, page });
+            }
           }
-          if (title.length > 800) break;
-          d.title = title
-          d.page = match[1];
-          sections.push(d);
-        }
-      };
+        });
+      }
+      
+      if (!sections.length) {
+      	match = /SUMÁRIO(.+[\.]{30}\s?\d+)/i.exec(textString)
+        let summaryText = match && match[1]
+        //results.push({summaryText})
+        let splits = summaryText.split(/([a-z].+?[\.]{10}.+?\d+)/i).filter((l) => /^\w.+\d+/.test(l) && l);
+        //results.push({splits})
+        let section = null
+        for (let i = 0; i < splits.length; i++) {
+          let d = {}
+          let title = splits[i]
+          let match = /([A-Z][A-Z].+[A-Z])\s/.exec(title)
+          if (match) section = match[1] 
+          match = /[.\s]{2,}(\d+)/.exec(title);
+          if (match) {
+            if (/\.gov\.br/.test(title)) {
+              title = title.replace(/^.+\.gov\.br/, '')
+            }
+            if (title.length > 800) break;
+            d.title = title.replace(/[\.]{2}.+/g, '').replace(/[\(\)]/g, '').replace(/[A-Z].+[A-Z]\s/, '').trim()
+            d.page = match[1];
+            d.section = section
+            if (!/[A-Z]$/.test(d.title)) { //don't create record for the sections
+            	sections.push(d);
+            }
+          }
+        };
+      }
+      // remove subsection record in case it has documents below it
+      let subsections = sections.filter(obj => obj.subsection === obj.title)
+      subsections.map(obj => {
+        let subsection = obj.subsection
+        let subsectionDocuments = sections.filter(obj => obj.subsection === subsection).length
+        if (subsectionDocuments > 1) {
+          //we dont need it so we filter it from the sections array
+          sections = sections.filter(obj => obj.title !== subsection)
+        } 
+      })
       //results.push({sections})
-
+      
       let locale = "br";
       for (let i = 0; i < sections.length; i++) {
         const obj = {URI: [], URL}
-        let { title, page } = sections[i];
+        let { section,subsection, title, page } = sections[i];
         let startPage = sections[i].page
         let endPage = i < sections.length - 1 ? sections[i + 1].page : 'end'
-        obj['title'] = title.replace(/[\.]{4}.+/g, '').replace(/[\(\)]/g, '').trim()
+        obj['section'] = section
+        obj['subsection'] = subsection
+        obj['title'] = title
         obj['disponibilaçao'] = formatDate(disponibilaçao)
         obj['publicação'] = formatDate(publicação)
         obj['ediçao Nº'] = ediçao
