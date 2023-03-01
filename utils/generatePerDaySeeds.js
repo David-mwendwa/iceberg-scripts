8a01ea1dcb5a7d5c1578ef1a40625f1ea7455db2
techdave <klatzdavie19@gmail.com>
2023-03-01 18:53:08 +0300

    utils: update per day seed generator

--- utils/generatePerDaySeeds.js
+++ utils/generatePerDaySeeds.js
@@ -1,16 +1,22 @@
 const moment = require('moment')
 
 function getSeeds() {
-    let start = moment();
-  	let end = moment().subtract(2, 'days');
-    start = moment('2022-01-28'); //Newest date
-    end = moment('2022-01-01');
+  	let from = moment().subtract(3, 'months');
+    let to = moment();
+  
+  	from = moment("2021-01-01");
+  	to = moment("2022-01-01");
+  
+  	if (from.isAfter(to))
+        [from, to] = [to, from];
+  
     let seeds = [];
-    let c = start.clone();
-    console.log(c)
-    while (c.isSameOrAfter(end)) {
-        if (c.isoWeekday() <= 5)
-            seeds.push(`https://dre.pt/web/guest/pesquisa-avancada?date=${c.format("YYYY-MM-DD")}`);
+    let c = to.clone();
+    while (c.isSameOrAfter(from)) {
+        if (c.isoWeekday() <= 5) {
+            let domain = ''
+          	seeds.push(`${domain}?from=${c.format("YYYY-MM-DD")}&to=${c.format("YYYY-MM-DD")}&page=1`)
+        }
         c = c.subtract(1, 'days');
     }
     return seeds;
